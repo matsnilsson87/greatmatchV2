496b6ac90fa17e4c353108d22cdbf3ee1ea94ec2
Kevin <kevin.svanback@gmail.com>
2020-01-03 14:27:57 +0100

    fixat sökfunktion

---
+++ gr8Match/Models/SeachBarViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using gr8Match.Models;
+
+namespace gr8Match.Models
+{
+    public class SeachBarViewModel
+    {
+        public List<User> User { get; set; }
+
+        public SeachBarViewModel()
+        {
+            User = new List<User>();
+        }
+    }
+}
