a0fe310cf047b2d33ed82efc3e776421de38421f
Kevin <kevin.svanback@gmail.com>
2020-01-03 15:28:12 +0100

    mats

--- gr8Match/Controllers/ProfileController.cs
+++ gr8Match/Controllers/ProfileController.cs
@@ -53,30 +53,72 @@ namespace gr8Match.Controllers
             var viewModel = new MyProfileViewModel
             {
                 MyImage = ctx.Images.Where(i => i.UserId == id).FirstOrDefault(),
-                FirstName = ctx.Database.SqlQuery<string>("Select FirstName from Users where Id =" + id ).FirstOrDefault(),
-                LastName = ctx.Database.SqlQuery<string>("Select LastName from Users where Id =" + id ).FirstOrDefault(),
+                FirstName = ctx.Database.SqlQuery<string>("Select FirstName from Users where Id =" + id).FirstOrDefault(),
+                LastName = ctx.Database.SqlQuery<string>("Select LastName from Users where Id =" + id).FirstOrDefault(),
                 MyInterests = ctx.Database.SqlQuery<string>("select Name from Interests join UserInterests on UserInterests.Interest=Interests.Id where UserInterests.UserId ='" + id.ToString() + "'").ToList(),
                 MyPosts = ctx.Posts.Where(i => i.WrittenTo == id).ToList()
             };
             return View(viewModel);
         }
 
+        public ActionResult OtherProfile(int id) {
+
+            
+            var ctx = new Gr8DbContext();
+            var viewModel = new OtherProfileViewModel
+            {
+                MyImage = ctx.Images.Where(i => i.UserId == id).FirstOrDefault(),
+                FirstName = ctx.Database.SqlQuery<string>("Select FirstName from Users where Id =" + id).FirstOrDefault(),
+                LastName = ctx.Database.SqlQuery<string>("Select LastName from Users where Id =" + id).FirstOrDefault(),
+                MyInterests = ctx.Database.SqlQuery<string>("select Name from Interests join UserInterests on UserInterests.Interest=Interests.Id where UserInterests.UserId ='" + id.ToString() + "'").ToList(),
+                MyPosts = ctx.Posts.Where(i => i.WrittenTo == id).ToList()
+            };
+            return View(viewModel);
+
+
+
+        }
         public ActionResult MyFriends()
         {
             var id = User.Identity.GetUserId();
             var ctx = new Gr8DbContext();
             var viewModel = new ProfileIndexViewModel
             {
-                Users = ctx.Database.SqlQuery<User>("Select * From Users Where Users.Id in (Select ToUser From FriendRequests Where FromUser in (Select Id From Users Where IdentityID = '" + id + "' and Accepted = 'True')) or Users.Id in (Select FromUser From FriendRequests Where ToUser in (Select Id From Users Where IdentityID = '" + id + "' and Accepted = 'True'))")
-                                                    .ToList(),
-                FriendsRequests = ctx.Database.SqlQuery<User>("Select * From Users Where Users.Id in (Select FromUser From FriendRequests Where ToUser in (Select Id From Users Where IdentityID = '" + id + "' and Accepted = 'False'))")
+                Users = ctx.Database.SqlQuery<User>("Select * " +
+                                                    "From Users " +
+                                                    "Join FriendRequests " +
+                                                    "on Users.Id = FriendRequests.ToUser " +
+                                                    "Where FriendRequests.FromUser in " +
+                                                    "(Select Id " +
+                                                    "From Users " +
+                                                    "Where IdentityID = '" + id + "')")
                                                     .ToList()
             };
             return View(viewModel);
         }
 
- 
+
+        public  ActionResult SearchBar(string search)
+        {
+            var ctx = new Gr8DbContext();
+            var lista = new SeachBarViewModel
+            {
+                User = ctx.Users.Where(x => x.FirstName.Contains(search) || search == null ).ToList()
+            };
+
+            return View(lista);
+        }
+
+        //public ActionResult SearchBar(string search)
+        //{
+        //    var ctx = new Gr8DbContext();
+        //    {
+        //        return View(ctx.Users.Where(x => x.FirstName.Contains(search) || search == null).ToList());
+        //    }
+
+        //}
 
 
     }
 }
+
---
+++ gr8Match/Models/OtherProfileViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gr8Match.Models
+{
+    public class OtherProfileViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Age { get; set; }
+        public List<string> MyInterests { get; set; }
+        public Image MyImage { get; set; }
+        public List<Posts> MyPosts { get; set; }
+
+        public OtherProfileViewModel()
+        {
+            FirstName = "";
+            LastName = "";
+            MyInterests = new List<string>();
+            MyImage = new Image();
+            MyPosts = new List<Posts>();
+        }
+    }
+}
